36b2a57b28b5635dfae933346f8d9ee732c1e7b5
zbeyens <zbeyens@udecode.io>
2023-12-27 15:33:51 +0000

    Merge remote-tracking branch 'origin/main'

--- tailwind.config.js
+++ tailwind.config.js
@@ -56,15 +56,16 @@ module.exports = {
       },
       fontFamily: {
         sans: ['var(--font-sans)', ...fontFamily.sans],
+        heading: ['var(--font-heading)', ...fontFamily.sans],
       },
       keyframes: {
         'accordion-down': {
-          from: { height: 0 },
+          from: { height: '0' },
           to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
+          to: { height: '0' },
         },
       },
       animation: {
